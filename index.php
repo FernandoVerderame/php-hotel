3f516176399c330a7485b94170b683410f0b0a1c
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-20 15:31:21 +0100

    added hotels table

--- index.php
+++ index.php
@@ -6,21 +6,57 @@ require __DIR__ . '/partials/data.php';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css' integrity='sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==' crossorigin='anonymous'/>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css'
+        integrity='sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=='
+        crossorigin='anonymous' />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css">
     <title>Hotels</title>
 </head>
+
 <body>
-    <ul>
-        <?php foreach ($hotels as $hotel) : ?>
-        <li>
-            <?= "{$hotel['name']} {$hotel['description']} {$hotel['parking']} {$hotel['vote']} {$hotel['distance_to_center']}" ?> 
-        </li>
-        <?php endforeach; ?>
-    </ul>
+    <section id="hotels-table">
+        <div class="container">
+            <h1 class="my-5">Hotels</h1>
+            <table class="table table-striped border border-black">
+                <thead class="text-center">
+                    <tr>
+                        <th class="text-start" scope="col">Nome</th>
+                        <th scope="col">Descrizione</th>
+                        <th scope="col">Parcheggio</th>
+                        <th scope="col">Voto</th>
+                        <th scope="col">Distanza dal centro</th>
+                    </tr>
+                </thead>
+                <tbody class="text-center">
+                    <?php foreach ($hotels as $hotel): ?>
+                        <tr>
+                            <td class="text-start">
+                                <?= $hotel['name'] ?>
+                            </td>
+                            <td>
+                                <?= $hotel['description'] ?>
+                            </td>
+                            <td>
+                                <?= $hotel['parking'] ?>
+                            </td>
+                            <td>
+                                <?= $hotel['vote'] ?>
+                            </td>
+                            <td>
+                                <?= $hotel['distance_to_center'] ?>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </section>
 </body>
+
 </html>
