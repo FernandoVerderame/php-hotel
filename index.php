d61e2ae557f42a2307f043f8b0bd1eb4323c469b
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-02-20 16:42:29 +0100

    added comments

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
 
+// Import hotels data
 require __DIR__ . '/partials/data.php';
 
+// Data variable
 $data = $_GET;
 
+// Parking filter
 if (isset($data['parking']) && !empty($data['parking'])) {
     $filter_parking = [];
 
@@ -16,6 +19,7 @@ if (isset($data['parking']) && !empty($data['parking'])) {
     $hotels = $filter_parking;
 }
 
+// Vote filter
 if (isset($data['vote']) && !empty($data['vote'])) {
     $vote = $data['vote'];
     $hotels = array_filter($hotels, fn($value) => $value['vote'] >= $vote);
@@ -39,16 +43,24 @@ if (isset($data['vote']) && !empty($data['vote'])) {
 </head>
 
 <body>
+
+    <!-- Hotels section -->
     <section id="hotels-table">
         <div class="container">
+
+            <!-- Header -->
             <header class="d-flex justify-content-between align-items-center">
                 <h1 class="my-5">Hotels</h1>
 
+                <!-- Filters -->
                 <?php include __DIR__ . '/template/filter.php' ?>
 
             </header>
+
+            <!-- Main -->
             <main>
 
+                <!-- Hotels table -->
                 <?php include __DIR__ . '/template/table.php' ?>
 
             </main>
